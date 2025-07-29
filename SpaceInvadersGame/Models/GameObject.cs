4d6cfd1e990c16addec954d32c81edbb869a978d
Kádymo <santanakadymo@gmail.com>
2025-07-29 11:56:02 -0300

    FEAT: Created generic class for game objects

---
+++ SpaceInvadersGame/Models/GameObject.cs
@@ -0,0 +1,13 @@
+﻿namespace SpaceInvadersGame.Models;
+
+public class GameObject
+{
+    public UIElement View { get; set; }
+    public object Model { get; set; }
+
+    public GameObject(UIElement view, object model)
+    {
+        View = view;
+        Model = model;
+    }
+}
