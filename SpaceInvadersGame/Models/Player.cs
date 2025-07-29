d4fc644d61504117b9adc2148c979181f9b569e7
Kádymo <santanakadymo@gmail.com>
2025-07-29 11:56:22 -0300

    FEAT: Model for player

---
+++ SpaceInvadersGame/Models/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace SpaceInvadersGame.Models;
+
+
+public class Player
+{
+    public double PositionX { get; set; }
+    public double PositionY { get; set; }
+    public double Health { get; set; }
+    public double Speed { get; set; }
+    
+    public event Action OnPlayerDied;
+
+    public void TakeDamage(int amount)
+    {
+        Health -= amount;
+        if (Health <= 0)
+        {
+            Console.WriteLine("Player Died!");
+            OnPlayerDied?.Invoke();
+        }
+    }
+}
