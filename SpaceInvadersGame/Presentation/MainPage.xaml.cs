789249be1222b9b627d76c3ee21e0184e423ae18
Kádymo <santanakadymo@gmail.com>
2025-07-29 22:27:20 -0300

    FEAT: Enemies grid renderized

--- SpaceInvadersGame/Presentation/MainPage.xaml.cs
+++ SpaceInvadersGame/Presentation/MainPage.xaml.cs
@@ -40,13 +40,15 @@ public sealed partial class MainPage : Page
             Width = 50,
             Height = 50,
         };
-        
+
         GameObject player = new GameObject(playerImage, new Player());
         _gameObjects.Add(player);
         _gameManager.AddGameObject(player);
         
         Canvas.SetZIndex(player.View, 10);
         GameCanvas.Children.Add(playerImage);
+        
+        CreateEnemiesGrid();
         
         CompositionTarget.Rendering += OnRendering;
     }
@@ -82,6 +84,48 @@ public sealed partial class MainPage : Page
             {
                 Canvas.SetLeft(go.View, player.PositionX);
                 Canvas.SetTop(go.View, player.PositionY);
+            }
+
+            if (go.Model is Enemy enemy)
+            {
+                Canvas.SetLeft(go.View, enemy.PositionX);
+                Canvas.SetTop(go.View, enemy.PositionY);;
+            }
+        }
+    }
+
+    private void CreateEnemiesGrid()
+    {
+        const int columns = 6;
+        const int rows = 4;
+        const int enemyWidth = 50;
+        const int enemyHeight = 50;
+        const double spacing = 10;
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Image enemyImage = new Image
+                {
+                    Source = new BitmapImage(new Uri("ms-appx:///Assets/Palmeiras_logo.svg.png")),
+                    Width = 50,
+                    Height = 50,
+                };
+                
+                GameObject enemyGameObject = new GameObject(enemyImage, new Enemy());
+
+                if (enemyGameObject.Model is Enemy enemyModel)
+                {
+                    enemyModel.PositionX = j * (enemyWidth + spacing);
+                    enemyModel.PositionY = i * (enemyHeight + spacing);
+                }
+                
+                _gameObjects.Add(enemyGameObject); 
+                _gameManager.AddGameObject(enemyGameObject);
+                
+                Canvas.SetZIndex(enemyGameObject.View, 10);
+                GameCanvas.Children.Add(enemyImage);
             }
         }
     }
