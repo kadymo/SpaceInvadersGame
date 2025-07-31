929e049c7c1542cdda68d42c2c92a0eefe2ac88c
Kádymo <santanakadymo@gmail.com>
2025-07-31 11:06:16 -0300

    FEAT: Class for Collision Event parameters

---
+++ SpaceInvadersGame/CollisionEventArgs.cs
@@ -0,0 +1,15 @@
+﻿using SpaceInvadersGame.Models;
+
+namespace SpaceInvadersGame;
+
+public class CollisionEventArgs
+{
+    public GameObject ProjectileGameObject { get; set; }
+    public GameObject EnemyGameObject { get; set; }
+
+    public CollisionEventArgs(GameObject projectileGameObject, GameObject enemyGameObject)
+    {
+        ProjectileGameObject = projectileGameObject;
+        EnemyGameObject = enemyGameObject;
+    }
+}
