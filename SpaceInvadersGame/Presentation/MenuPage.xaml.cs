6873a61ca6785f0fb73129d372f084090c540d00
Kádymo <santanakadymo@gmail.com>
2025-08-05 14:26:37 -0300

    FEAT: Added MenuPage and navigation to MainPage via start game button

---
+++ SpaceInvadersGame/Presentation/MenuPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace SpaceInvadersGame.Presentation;
+
+public sealed partial class MenuPage : Page
+{
+    public MenuPage()
+    {
+        this.InitializeComponent();
+    }
+
+    private void StartGameButton_Click(object sender, RoutedEventArgs e)
+    {
+        this.Frame.Navigate(typeof(MainPage));
+    }
+}
+
