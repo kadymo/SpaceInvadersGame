bb4e6086e49cf156898356becfc0c6dbd311542e
Kádymo <santanakadymo@gmail.com>
2025-08-04 23:48:19 -0300

    FEAT: Obstacle model and image element

---
+++ SpaceInvadersGame/Models/Obstacle.cs
@@ -0,0 +1,8 @@
+﻿namespace SpaceInvadersGame.Models;
+
+public class Obstacle
+{
+    public double PositionX { get; set; }
+    public double PositionY { get; set; }
+    public double Health { get; set; } = 100;
+}
