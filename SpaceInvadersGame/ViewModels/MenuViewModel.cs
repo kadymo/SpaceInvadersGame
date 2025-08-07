c13e66945676e9babdbe667cf73f0efbf37f01bd
Kádymo <santanakadymo@gmail.com>
2025-08-07 08:38:01 -0300

    FEAT: UsernameTextBox data-binding and StartGameButton disabled status

---
+++ SpaceInvadersGame/ViewModels/MenuViewModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SpaceInvadersGame.ViewModels;
+
+public partial class MenuViewModel : ObservableObject
+{
+    [ObservableProperty] 
+    [NotifyPropertyChangedFor(nameof(IsStartButtonEnabled))]
+    private string _username;
+    
+    public bool IsStartButtonEnabled => !string.IsNullOrWhiteSpace(_username);
+}
