c2b40b6b05aad4c714049860d3573b1f485317d8
Kádymo <santanakadymo@gmail.com>
2025-08-10 17:36:32 -0300

    FEAT: Changing player initial position

--- SpaceInvadersGame/Models/Player.cs
+++ SpaceInvadersGame/Models/Player.cs
@@ -3,8 +3,8 @@
 
 public class Player
 {
-    public double PositionX { get; set; }
-    public double PositionY { get; set; }
+    public double PositionX { get; set; } = 300;
+    public double PositionY { get; set; } = 450;
     public double Health { get; set; } = 100;
     public double Speed { get; set; } = 200;
     
