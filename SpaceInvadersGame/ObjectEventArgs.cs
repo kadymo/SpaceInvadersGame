09857ab0684e69dc9a9dff868fc3085eef842582
Kádymo <santanakadymo@gmail.com>
2025-08-10 17:38:28 -0300

    FEAT: Event args for events between ViewModel -> Code-Behind

---
+++ SpaceInvadersGame/ObjectEventArgs.cs
@@ -0,0 +1,25 @@
+﻿using SpaceInvadersGame.Models;
+
+namespace SpaceInvadersGame;
+
+public class ObjectEventArgs
+{
+    public GameObject GameObject { get; set; }
+    public Image ImageElement { get; set; }
+    
+    public ObjectEventArgs(GameObject gameObject)
+    {
+        GameObject = gameObject;
+    }
+    
+    public ObjectEventArgs(Image imageElement)
+    {
+        ImageElement = imageElement;
+    }
+    
+    public ObjectEventArgs(GameObject gameObject, Image imageElement)
+    {
+        GameObject = gameObject;
+        ImageElement = imageElement;
+    }
+}
