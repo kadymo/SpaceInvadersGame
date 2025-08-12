7398f656ced4c65e91d1e611e9c218f136dbcf65
Kádymo <santanakadymo@gmail.com>
2025-08-11 23:07:09 -0300

    FEAT: Reducing projectile speed

--- SpaceInvadersGame/Models/Projectile.cs
+++ SpaceInvadersGame/Models/Projectile.cs
@@ -1,12 +1,13 @@
 ﻿using SpaceInvadersGame.Models.Enums;
+using SpaceInvadersGame.Models.Interfaces;
 
 namespace SpaceInvadersGame.Models;
 
-public class Projectile
+public class Projectile : IPositionable
 {
     public double PositionX { get; set; }
     public double PositionY { get; set; }
     public double Health { get; set; } = 100;
-    public double Speed { get; set; } = 300;
+    public double Speed { get; set; } = 150;
     public ProjectileFirer Firer { get; set; }
 }
