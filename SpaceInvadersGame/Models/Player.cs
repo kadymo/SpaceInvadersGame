94d78e87585e2d81c29cb63d0d4d2ce778cf2d3a
Kádymo <santanakadymo@gmail.com>
2025-08-19 00:40:17 -0300

    FEAT: Increase player speed

--- SpaceInvadersGame/Models/Player.cs
+++ SpaceInvadersGame/Models/Player.cs
@@ -8,7 +8,7 @@ public class Player : IPositionable
     public double PositionX { get; set; } = 300;
     public double PositionY { get; set; } = 450;
     public double Health { get; set; } = 100;
-    public double Speed { get; set; } = 200;
+    public double Speed { get; set; } = 220;
     
     public int Lifes { get; set; } = 3;
     public int Score { get; set; } = 0;
