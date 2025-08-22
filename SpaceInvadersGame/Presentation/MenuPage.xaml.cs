bf33ee1bf67d64966d43e2f0125ab47397f31efb
Kádymo <santanakadymo@gmail.com>
2025-08-22 17:35:23 -0300

    FEAT: Score ranking dialog

--- SpaceInvadersGame/Presentation/MenuPage.xaml.cs
+++ SpaceInvadersGame/Presentation/MenuPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Microsoft.UI.Xaml;
@@ -25,6 +26,58 @@ public sealed partial class MenuPage : Page
     private void StartGameButton_Click(object sender, RoutedEventArgs e)
     {
         this.Frame.Navigate(typeof(MainPage), UsernameTextbox);
+    }
+    
+    private async void ShowScoreRankingDialog(object sender, RoutedEventArgs e)
+    {
+        var scoreListLines = await GetScoreAsync();
+        var stringBuilder = new StringBuilder();
+        
+        foreach (var line in scoreListLines)
+        {
+            if (string.IsNullOrWhiteSpace(line)) continue;
+            stringBuilder.Append(line);
+            stringBuilder.Append(Environment.NewLine);
+        }
+        
+        ContentDialog scoreRankingDialog = new ContentDialog
+        {
+            Title = "Score - Ranking",
+            Content = stringBuilder.ToString(),
+            SecondaryButtonText = "Resetar",
+            CloseButtonText = "Fechar",
+            XamlRoot = this.XamlRoot,
+        };
+        
+        ContentDialogResult result = await scoreRankingDialog.ShowAsync();
+        if (result == ContentDialogResult.Secondary)
+        {
+            await ClearLocalFolderAsync();
+            scoreRankingDialog.Content = await GetScoreAsync();
+        }
+    }
+    
+    private async Task<IList<string>> GetScoreAsync()
+    {
+        try
+        {
+            var storageFolder = ApplicationData.Current.LocalFolder;
+            var fileName = "Score.txt";
+            var scoreFile = await storageFolder.GetFileAsync(fileName);
+            
+            var lines = await FileIO.ReadLinesAsync(scoreFile);
+            return lines;
+        } catch(Exception ex) {}
+
+        return new List<string>();
+    }
+    
+    public async Task ClearLocalFolderAsync()
+    {
+        var storageFolder = ApplicationData.Current.LocalFolder;
+        var fileName = "Score.txt";
+        var scoreFile = await storageFolder.GetFileAsync(fileName);
+        await FileIO.WriteTextAsync(scoreFile, string.Empty);
     }
 }
 
