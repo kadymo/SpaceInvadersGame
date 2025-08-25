966933b17951247326728d277b52c9787bca429c
Kádymo <santanakadymo@gmail.com>
2025-08-25 01:41:04 -0300

    FIX: Player initial position outside screen bounds

--- SpaceInvadersGame/Models/Player.cs
+++ SpaceInvadersGame/Models/Player.cs
@@ -6,7 +6,7 @@ namespace SpaceInvadersGame.Models;
 public class Player : IPositionable
 {
     public double PositionX { get; set; } = 300;
-    public double PositionY { get; set; } = 450;
+    public double PositionY { get; set; } = 425;
     public double Health { get; set; } = 100;
     public double Speed { get; set; } = 220;
     
